c2934c1ff6e74ad142232428db4f3938277f7169
Bilal989 <bfhamid@gmail.com>
2019-12-08 18:50:03 +0500

    2.35 get authenticated user

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const functions = require('firebase-functions');
 const app = require('express')()
 // const firebase = require('firebase')
 const { getAllScreams, postOneScream } = require('./handlers/screams')
-const {signup, login, uploadImage, addUserDetails} = require('./handlers/users')
+const {signup, login, uploadImage, getAuthenticatedUser,
+    addUserDetails} = require('./handlers/users')
 const FBAuth = require('./util/FBAuth')
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
@@ -11,8 +12,11 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 
 app.get('/screams', getAllScreams)
 app.post('/scream', FBAuth, postOneScream)
-app.post('/user', FBAuth, addUserDetails)
+app.get('/scream/:screamId', getScream)
 
+
+app.post('/user', FBAuth, addUserDetails)
+app.get('/user', FBAuth, getAuthenticatedUser)
 app.post('/signup', signup)
 app.post('/login', login)
 app.post('/user/image', FBAuth, uploadImage)
