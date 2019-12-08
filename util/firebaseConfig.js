3b9b6ab22b2c79793605237c77798ad3fe9df54d
Bilal989 <bfhamid@gmail.com>
2019-12-08 15:42:48 +0500

    1.46 code refactoring to files

---
+++ util/firebaseConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyB1PDceHct0rgHrJ1BEizzQUiwXmQPu5zQ",
+    authDomain: "fcc-12hr-fb-clone.firebaseapp.com",
+    databaseURL: "https://fcc-12hr-fb-clone.firebaseio.com",
+    projectId: "fcc-12hr-fb-clone",
+    storageBucket: "fcc-12hr-fb-clone.appspot.com",
+    messagingSenderId: "1026327730125",
+    appId: "1:1026327730125:web:0e010766222c570d615684"
+  };
