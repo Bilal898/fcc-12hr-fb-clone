50e317f0ce9ad6e00684659131cf9e8e8946403c
Bilal989 <bfhamid@gmail.com>
2019-12-09 19:10:58 +0500

    2.56 add new comment to scream

--- handlers/screams.js
+++ handlers/screams.js
@@ -68,3 +68,31 @@ exports.getScream = (req, res) => {
             res.status(500).json({error: err.code})
         })
 }
+
+exports.commentOneScream = (req, res) => {
+    if(req.body.body.trim() === '') return res.status(400).json({error: 'must not be empty'})
+
+    const newComment = {
+        body: req.body.body,
+        createdAt: new Date().toISOString(),
+        screamId: req.params.screamId,
+        userHandle: req.user.handle,
+        userImage: req.user.imageUrl
+    }
+
+    db.doc(`/screams/${req.params.screamId}`).get()
+        .then(doc => {
+            if(!doc.exists){
+                return res.status(404).json({error: 'scream not found'})
+            }
+            return db.collection('comments').add(newComment)
+        })
+        .then(() => {
+            res.json(newComment)
+        })
+        .catch(err => {
+            console.error(err)
+            res.status(500).json({error: err.code})
+        })
+
+}
--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const functions = require('firebase-functions');
 const app = require('express')()
 // const firebase = require('firebase')
-const { getAllScreams, postOneScream, getScream } = require('./handlers/screams')
+const { getAllScreams, postOneScream, getScream, commentOneScream } = require('./handlers/screams')
 const {signup, login, uploadImage, getAuthenticatedUser,
     addUserDetails} = require('./handlers/users')
 const FBAuth = require('./util/FBAuth')
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//     response.send("Hello from Firebase!");
+// });
 
 app.get('/screams', getAllScreams)
 app.post('/scream', FBAuth, postOneScream)
 app.get('/scream/:screamId', getScream)
-
+app.post('/scream/:screamId/comment', FBAuth, commentOneScream)
 
 app.post('/user', FBAuth, addUserDetails)
 app.get('/user', FBAuth, getAuthenticatedUser)
