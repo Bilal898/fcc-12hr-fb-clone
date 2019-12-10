d5d13fa5db687150454d51d2ce55d3eaa6de1b0d
Bilal989 <bfhamid@gmail.com>
2019-12-10 11:21:02 +0500

    3.17 like unlike

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const functions = require('firebase-functions');
 const app = require('express')()
 // const firebase = require('firebase')
-const { getAllScreams, postOneScream, getScream, commentOneScream } = require('./handlers/screams')
+const { getAllScreams, postOneScream, getScream, 
+    commentOneScream, likeScream, unlikeScream } = require('./handlers/screams')
 const {signup, login, uploadImage, getAuthenticatedUser,
     addUserDetails} = require('./handlers/users')
 const FBAuth = require('./util/FBAuth')
@@ -14,6 +15,8 @@ app.get('/screams', getAllScreams)
 app.post('/scream', FBAuth, postOneScream)
 app.get('/scream/:screamId', getScream)
 app.post('/scream/:screamId/comment', FBAuth, commentOneScream)
+app.get('/scream/:screamId/like', FBAuth, likeScream)
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream)
 
 app.post('/user', FBAuth, addUserDetails)
 app.get('/user', FBAuth, getAuthenticatedUser)
